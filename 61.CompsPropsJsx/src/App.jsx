09bbf1464cc9fd4ecefb82da1511b2233e786503
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-19 23:42:02 +0530

    code added

--- 61.CompsPropsJsx/src/App.jsx
+++ 61.CompsPropsJsx/src/App.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { useState } from "react";
+import UserCard from "./components/UserCard";
 
 function App() {
+  const [state, setState] = useState(true);
   return (
     <div>
-      <h1>React Comps</h1>
+      {state ? (
+        <div className="container">
+          <UserCard />
+          <UserCard />
+          <UserCard />
+        </div>
+      ) : (
+        <h1>React Comps</h1>
+      )}
     </div>
   );
 }
--- 61.CompsPropsJsx/src/components/UserCard.jsx
+++ 61.CompsPropsJsx/src/components/UserCard.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
+import "./UserCard.css";
 
 function UserCard() {
   return (
-    <div>
-      
+    <div className="user-container">
+      <p id="user-name">Deepak</p>
+      <img
+        id="user-img"
+        src="https://images.unsplash.com/photo-1741762764258-8f9348bdf186?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8NHx8fGVufDB8fHx8fA%3D%3D"
+        alt=""
+      />
+      <p id="user-desc">Description of Deepak</p>
     </div>
-  )
+  );
 }
 
-export default UserCard
+export default UserCard;
