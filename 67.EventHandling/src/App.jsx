b86fafd49248d75e200066265ca6e45fe142a56a
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-21 22:47:35 +0530

    handleMouseOver func code added

--- 67.EventHandling/src/App.jsx
+++ 67.EventHandling/src/App.jsx
@@ -4,8 +4,13 @@ function App() {
   const handleClick = () => {
     alert("I am Clicked");
   };
+
+  const handleMouseOver = () => {
+    alert("handleMouseOver is being done");
+  };
   return (
     <div>
+      <p onMouseOver={handleMouseOver}>I am Para Tag</p>
       <button onClick={handleClick}>Event Handling</button>
     </div>
   );
