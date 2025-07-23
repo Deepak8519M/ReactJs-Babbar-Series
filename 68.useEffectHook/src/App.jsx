4d4f943e2cad2a9c929fe00e2a59dca26c50c527
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-23 23:45:47 +0530

    code added

--- 68.useEffectHook/src/App.jsx
+++ 68.useEffectHook/src/App.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const [total, setTotal] = useState(0);
+
+  useEffect(() => {
+    alert("I will show up every time count is rerendered");
+  }, [count]);
+
+  const handlClick = () => {
+    setCount(count + 1);
+  };
+
+  const handleTotalClick = () => {
+    setTotal(total + 1);
+  };
+
   return (
     <div>
-      <h1>Use Effect Hook</h1>
+      <h1>{count}</h1>
+      <button onClick={handlClick}>Click</button>
+      <br />
+      <h1>{total}</h1>
+      <button onClick={handleTotalClick}>Click</button>
     </div>
   );
 }
