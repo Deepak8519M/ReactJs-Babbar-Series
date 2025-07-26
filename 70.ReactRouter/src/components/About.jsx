ca6c0984159be177c94e65d05950a7b1bcc06ccb
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-26 22:07:40 +0530

    code added

--- 70.ReactRouter/src/components/About.jsx
+++ 70.ReactRouter/src/components/About.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const About = () => {
+  const navigate = useNavigate();
+
+  const handler = () => {
+    navigate("/dashboard");
+  };
   return (
     <div>
       <h1>About</h1>
+      <button onClick={handler}>Move to About Page</button>
     </div>
   );
 };
--- 70.ReactRouter/src/components/DashBoard.jsx
+++ 70.ReactRouter/src/components/DashBoard.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const DashBoard = () => {
+  const navigate = useNavigate();
+
+  const handler = () => [navigate("/")];
   return (
     <div>
       <h1>DashBoard</h1>
+      <button onClick={handler}>Move to About Page</button>
     </div>
   );
 };
