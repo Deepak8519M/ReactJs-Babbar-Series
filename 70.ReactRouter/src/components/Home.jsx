adf09554c4ec923974219b378edf110e67fc97a1
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-26 22:01:28 +0530

    useNav code added

--- 70.ReactRouter/src/components/Home.jsx
+++ 70.ReactRouter/src/components/Home.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handler = () => {
+    navigate("/about");
+  };
   return (
     <div>
       <h1>Home</h1>
+      <button onClick={handler}>Move to About Page</button>
     </div>
   );
 };
