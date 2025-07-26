cae55a03df14f36b9b2beeef15b0f55d1906a9c5
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-26 21:26:07 +0530

    code added

--- 70.ReactRouter/src/App.jsx
+++ 70.ReactRouter/src/App.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
+import Home from "./components/Home";
+import About from "./components/About";
+import DashBoard from "./components/DashBoard";
 
 const router = createBrowserRouter([
   {
-    path:""
-  }
-])
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+  {
+    path: "/dashboard",
+    element: <DashBoard />,
+  },
+]);
 
 function App() {
   return (
