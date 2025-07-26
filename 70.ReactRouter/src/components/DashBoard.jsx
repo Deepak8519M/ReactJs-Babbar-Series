f78fa7f0404828bb30396dee72884c653db592ef
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-26 23:19:41 +0530

    code added with Outlet

--- 70.ReactRouter/src/components/DashBoard.jsx
+++ 70.ReactRouter/src/components/DashBoard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const DashBoard = () => {
   const navigate = useNavigate();
@@ -9,6 +9,7 @@ const DashBoard = () => {
     <div>
       <h1>DashBoard</h1>
       <button onClick={handler}>Move to About Page</button>
+      <Outlet />
     </div>
   );
 };
