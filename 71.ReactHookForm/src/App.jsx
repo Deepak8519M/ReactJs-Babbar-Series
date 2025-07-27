6e3c0ec798f51e140c33ae84931a3f7848b142a4
Deepak Mallareddy <deepak8519m@gmail.com>
2025-07-27 16:03:42 +0530

    code added

--- 71.ReactHookForm/src/App.jsx
+++ 71.ReactHookForm/src/App.jsx
@@ -1,9 +1,39 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const App = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log("Submitting the Form", data);
+  };
+
   return (
     <div>
-      <h1>React Hook Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label>First Name</label>
+          <input type="text" {...register("firstName")} />
+        </div>
+        <br />
+        <div>
+          <label>Middle Name</label>
+          <input type="text" {...register("middleName")} />
+        </div>
+        <br />
+        <div>
+          <label>Middle Name</label>
+          <input type="text" {...register("lastName")} />
+        </div>
+
+        <br />
+        <input type="submit" />
+      </form>
     </div>
   );
 };
